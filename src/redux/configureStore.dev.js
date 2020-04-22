caccb10ca75d822563223be9e4926fc2556aef6c
Jonathan <jonathanferre2@gmail.com>
2020-04-21 21:32:24 -0300

    Production Builds

---
+++ src/redux/configureStore.dev.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducers";
+import reduxInmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
+
+export default function configureStore(initialState) {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose; // add support for Redux dev tools
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(thunk, reduxInmutableStateInvariant()))
+  );
+}
